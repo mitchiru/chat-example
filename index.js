ca29fa92cde7cdc7915f4cbb0c0df3abf0a77d04
Michael Fritz <mitchiru@gmx.de>
2022-01-04 11:14:27 +0100

    assets

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var express = require('express');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -6,6 +7,13 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+
+
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
